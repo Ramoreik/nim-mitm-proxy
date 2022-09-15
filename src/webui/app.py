a2b0578e8e2774b7fb7cf7a6762136a56cca576c
gh0st <gh0st@pop-os.localdomain>
2022-09-15 00:04:43 -0400

    added click interface

--- src/webui/app.py
+++ src/webui/app.py
@@ -4,7 +4,7 @@ import glob
 import sqlite3
 import click
 from flask import (Flask, render_template, 
-				request, redirect, g, abort)
+                request, redirect, g, abort)
 
 INTERACTIONS_DB = "../interactions.db"
 
@@ -27,13 +27,13 @@ app = Flask(__name__)
 ##
 
 def query_domains(query: str) -> []:  
-	db = get_db()
-	c = db.cursor()
-	res = c.execute(
-		"SELECT rowid, host, port FROM interaction WHERE host LIKE :query",
-		 {"query": "%" + query + "%"}).fetchall()
-	c.close()
-	return res
+    db = get_db()
+    c = db.cursor()
+    res = c.execute(
+        "SELECT rowid, host, port FROM interaction WHERE host LIKE :query",
+         {"query": "%" + query + "%"}).fetchall()
+    c.close()
+    return res
 
 
 def get_db():
@@ -44,43 +44,43 @@ def get_db():
 
 @app.route('/', methods=["GET"])
 def home():
-	return redirect("/show")
+    return redirect("/show")
 
 
 @app.route("/show", methods=["GET"])
 def show():
-	db = get_db()
-	c = db.cursor()
-	res = c.execute("SELECT DISTINCT host FROM interaction").fetchall()
-	c.close()
-	return render_template("home.html", domains=res)
+    db = get_db()
+    c = db.cursor()
+    res = c.execute("SELECT DISTINCT host FROM interaction").fetchall()
+    c.close()
+    return render_template("home.html", domains=res)
 
 
 @app.route("/show/<query>", methods=["GET"])
 def show_search(query: str):
-	return render_template("home.html", 
-		interactions=query_domains(query), q=query), 200
+    return render_template("home.html", 
+        interactions=query_domains(query), q=query), 200
 
 
 @app.route("/get/interaction", methods=["GET"])
 def get_interaction():
-	rid = request.args.get("target", None)
-	if rid is None: abort(404)
-	db = get_db()
-	c = db.cursor()
-	interaction = db.execute(
-		"SELECT request, response from interaction WHERE rowid=:rid", 
-		{"rid": rid}).fetchone()
-	c.close()
-	return render_template("interaction.html", interaction=interaction), 200
+    rid = request.args.get("target", None)
+    if rid is None: abort(404)
+    db = get_db()
+    c = db.cursor()
+    interaction = db.execute(
+        "SELECT request, response from interaction WHERE rowid=:rid", 
+        {"rid": rid}).fetchone()
+    c.close()
+    return render_template("interaction.html", interaction=interaction), 200
 
 
 @app.route("/clean", methods=["POST"])
 def clean():
-	if os.path.exists(app.config["db"]):
-		app.logger.info(f"[*] Cleaning {app.config['db']}.")
-		os.unlink(app.config["db"])
-	return redirect("/")
+    if os.path.exists(app.config["db"]):
+        app.logger.info(f"[*] Cleaning {app.config['db']}.")
+        os.unlink(app.config["db"])
+    return redirect("/")
 
 
 @app.teardown_appcontext
@@ -92,14 +92,18 @@ def teardown_db(app):
 
 @click.command()
 @click.option('--interface', '-i', default="127.0.0.1", 
-	          help="Specify the interface to serve on.")
+              help="Specify the interface to serve on.")
 @click.option('--port', '-p', default=1337, 
-		      help="Specify the port to listen on.")
+              help="Specify the port to listen on.")
 @click.option('--db', default=INTERACTIONS_DB, 
-			  help="Specify the db to use as source.")
+              help="Specify the db to use as source.")
 def main(interface, port, db):
-	app.config["db"] = db
-	app.run(interface, port)
+    if not os.path.exists(db): 
+        print("[!] DB does not exist.")
+        print("[!] Exiting.")
+        exit(1)
+    app.config["db"] = db
+    app.run(interface, port)
 
 if __name__ == "__main__":
-	main()
+    main()
